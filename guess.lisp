cf079e05d7fd3510c375fadd4568b99748fbd936
Shady <shady@shadyproject.net>
2013-10-25 08:00:35 -0700

    guessing game

---
+++ guess.lisp
@@ -0,0 +1,13 @@
+(defparameter *big* 100)
+(defparameter *small* 1)
+
+(defun guess-my-number ()
+  (ash (+ *small* *big*) -1))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
